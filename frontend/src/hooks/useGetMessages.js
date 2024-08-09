0909740bf66f3094b390ca9b86e9e954291f3a52
ashishsinghrana39@gmail.com <106158861+ashishsingh2003@users.noreply.github.com>
2024-08-09 14:47:54 +0530

    committed

---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useGetMessages = () => {
+
+	const { messages, setMessages, selectedConversation } = useConversation();
+
+	useEffect(() => {
+		const getMessages = async () => {
+		
+			try {
+				const res = await fetch(`/api/messages/${selectedConversation._id}`);
+				const data = await res.json();
+				if (data.error) throw new Error(data.error);
+				setMessages(data);
+			} catch (error) {
+				toast.error(error.message);
+			} finally {
+				
+			}
+		};
+
+		if (selectedConversation?._id) getMessages();
+	}, [selectedConversation?._id, setMessages]);
+
+	return { messages};
+};
+export default useGetMessages;
